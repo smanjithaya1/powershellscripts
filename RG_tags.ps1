68cf9bee46e1e77109a004a5a84cb3fdb1aaafbe
smanjithaya1 <101131431+smanjithaya1@users.noreply.github.com>
2022-03-16 17:09:07 +0530

    Update RG_tags.ps1

--- RG_tags.ps1
+++ RG_tags.ps1
@@ -1,9 +1,16 @@
-# Add the tags in this format, in the below variable $tags = @{"Dept"="Finance"; "Status"="Normal"}
-$tags = @{}
-foreach ( $subscription in Get-Subscriptions ) {
-  Write-Host "$subscription.Name is the current subscription"
-  Select-AzureSubscription -Current -SubscriptionName $subscription.Name
-  foreach ( $resourcegroup in Get-AzResourceGroup ) {
-    New-AzTag -ResourceId $resourcegroup.ResourceId -tag $tags
-  }
+#Used to continue on error
+$ErrorActionPreference = 'SilentlyContinue'
+
+#It is assumed that csv file is named as subscriptions.csv
+Import-Csv .\subscriptions.csv | ForEach-Object {
+    Write-Host "$($_.Name), is the subscription and $($_.tag), is the tag."
+	$subscription_Name = $_.Name
+	$tag = $_.tag
+	$tags = @{"Application"=$tag}
+	Select-AzureSubscription -Current -SubscriptionName $subscription_Name
+	foreach ( $resourcegroup in Get-AzResourceGroup ) {
+		#Update-Aztag will update the existing tags and merge it. It will also ignore if the tag is already applied
+	    Update-AzTag -ResourceId $resource.id -Tag $tags -Operation Merge
+	}
+	
 }
