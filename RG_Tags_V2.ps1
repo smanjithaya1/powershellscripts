b3639d01ad84852d27fc881694b4a4d199c5b12e
smanjithaya1 <101131431+smanjithaya1@users.noreply.github.com>
2022-03-23 20:33:49 +0530

    Create RG_Tags_V2.ps1

---
+++ RG_Tags_V2.ps1
@@ -0,0 +1,14 @@
+#Used to continue on error
+$ErrorActionPreference = 'SilentlyContinue'
+
+#It is assumed that csv file is named as subscriptions.csv
+Import-Csv .\source.csv | ForEach-Object {
+  Write-Host "$($_.Name), is the subscription, $_.rg is the resource group and $($_.tag), is the tag."
+	$subscription_Name = $_.Name
+	$resource_group = $_.rg
+  $tag = $_.tag
+	$tags = @{"Application"=$tag}
+	Select-AzureSubscription -Current -SubscriptionName $subscription_Name
+  resource = Get-AzResourceGroup -Name $resource_group
+  Update-AzTag -ResourceId $resource.id -Tag $tags -Operation Merge
+  }
